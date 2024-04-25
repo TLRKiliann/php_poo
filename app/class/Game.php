dc06d42bee4872cb22a47716a34420988d6b46d2
TLRKiliann <philogenie@protonmail.com>
2024-04-25 23:57:56 +0200

    some changes

--- app/class/Game.php
+++ app/class/Game.php
@@ -4,18 +4,26 @@
     class Game
     {
         private $username;
+        private $atk;
         private $score;
 
-        public function __construct(string $u ,int $s) {
+        public function __construct(string $u , int $a, int $s) {
             $this->username = $u;
+            $this->atk = $a;
             $this->score = $s;
         }
 
-        public function getData(): void {
-            echo $this->username . " " . $this->score;
+        public function get_atk(): int {
+            $this->atk += 20;
+            return $this->atk;
         }
 
-        public function getScore(): int {
+        public function get_data(): void {
+            echo '<p>Player: ' . $this->username . ' Score: ' . $this->score . '</p>';
+        }
+
+        public function get_score(): int {
+            $this->score -= $this->atk;
             return $this->score;
         }
     }
