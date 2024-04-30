dade2ec4871252900a53dfc4dd703c0da70d213c
TLRKiliann <philogenie@protonmail.com>
2024-04-30 17:32:55 +0200

    pdo created

---
+++ app/pages/home.php
@@ -0,0 +1,46 @@
+<?php
+    require_once('../includes/cookie_helper.php');
+
+    //head
+    $title = "Home Page";
+    $style = "../public/css/styles.css";
+    $favicon = "../images/favicon.png";
+
+    //routes
+    $login = '../public/index.php';
+    $about = 'about.php';
+    $home = 'home.php';
+    $contact = 'contact.php';
+    $str_session_name = get_username_from_cookie();
+
+    $pdo = new PDO('mysql:host=192.168.18.9;port:3306;dbname=mytable', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $res = $pdo->query('SELECT * FROM articles');
+    var_dump($res->fetchAll(PDO::FETCH_OBJ));
+    var_dump($res->fetchAll());
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <?php require_once '../html/Head.php'; ?>
+    </head>
+
+    <body>
+
+        <header>
+            <nav class="navbar">
+                <?php require_once '../html/Navbar.php'; ?>
+            </nav>   
+        </header>
+
+        <main>
+            <h1>Home</h1>
+        </main>
+
+    </body>
+
+</html>
