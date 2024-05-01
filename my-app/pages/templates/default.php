978e266f58aaf20359ae766deb12c6a3c38ccc35
TLRKiliann <philogenie@protonmail.com>
2024-05-01 09:33:28 +0200

    added to match with pages (index.php)

---
+++ my-app/pages/templates/default.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <?php require_once '../html/Head.php'; ?>
+    </head>
+
+    <body>
+
+        <header>
+            <nav class="navbar">
+                <?php require_once '../html/Navbar.php'; ?>
+            </nav>   
+        </header>
+
+
+        <head>
+            <?php require_once '../html/Head.php'; ?>
+        </head>
+
+
+        <main>
+            <h1>Contact</h1>
+        
+            <section>
+
+                <?php $content; ?>
+            
+            </section>
+
+        </main>
+
+    </body>
+
+</html>
