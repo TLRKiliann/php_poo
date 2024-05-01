2eb5edce6608e640c2cc125b2da7c8ad3e186f49
TLRKiliann <philogenie@protonmail.com>
2024-05-01 12:43:05 +0200

    autoload improved

--- my-app/app/Autoloader.php
+++ my-app/app/Autoloader.php
@@ -7,21 +7,12 @@
             spl_autoload_register(array(__CLASS__, 'autoload'));
         }
 
-        /* static function autoload($class) {
+        static function autoload($class) {
             if (strpos($class, __NAMESPACE__ . '\\') === 0) {
-                $class = str_replace(__NAMESPACE__ . '', $class);
+                $class = str_replace(__NAMESPACE__ . '\\', '', $class);
                 $class = str_replace('\\', '/', $class);
                 require __DIR__ . '/' . $class . '.php';
             }
-        } */
-
-        /**
-         * @param $class string
-        */
-        static function autoload($class) {
-            $class = str_replace('App\\', '', $class);
-            $class = str_replace('\\', '/', $class);
-            require '../app/' . $class . '.php';
         }
     }
 ?>
