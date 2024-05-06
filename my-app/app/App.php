f923722b6cda1b9852d229d0754e1c7482b06f58
TLRKiliann <philogenie@protonmail.com>
2024-05-06 11:19:37 +0200

    relation with home & article page

---
+++ my-app/app/App.php
@@ -0,0 +1,24 @@
+<?php
+
+    namespace App;
+
+    class App
+    {
+        const DB_NAME = 'mytable';
+        const DB_HOST = '192.168.18.9';
+        const DB_PORT = 3306;
+        const DB_USER = 'koala33';
+        const DB_PASS = 'Ko@l@tr3379';
+
+        private static $database;
+
+        public static function getDatabase()
+        {
+            if (self::$database === null) {
+                self::$database = new Database(self::DB_NAME, self::DB_HOST, self::DB_PORT, 
+                self::DB_USER, self::DB_PASS);
+            }
+            return self::$database;
+        }
+    }
+?>
